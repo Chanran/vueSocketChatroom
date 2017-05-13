165a047c29b4a6788f202f0900bf3af4662aa42b
blue <rong650751@163.com>
2017-05-14 00:12:32 +0800

    取得socket连接者的ip、端口

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const crypto = require('crypto');
+// const crypto = require('crypto');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/login', (req, res) => {
   let username = req.query.username || req.params.username;
-  // const md5 = crypto.createHash('md5');
-  // md5.digest('hex');
+  let ip = req.connection.remoteAddress;
+  let port = req.connection.remotePort;
 
-  res.cookie('username', username, {
-    secure: false,
-    expires: new Date(Date.now() + (1000 * 60 * 60 * 24)), // cookie保存一天时间
-    httpOnly: true,
+  res.cookie('ioUser',
+    {
+      username,
+      ip,
+      port,
+    },
+    {
+      secure: false,
+      expires: new Date(Date.now() + (1000 * 60 * 60 * 24)), // cookie保存一天时间
+      httpOnly: true,
+    });
+  res.json({
+    msg: 'success',
+    code: '200',
   });
-  res.json({});
 });
 
 module.exports = router;
