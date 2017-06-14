8f2c035e1a722f58b7438733f9152789358e8b11
blue <rong650751@163.com>
2017-06-14 11:18:13 +0800

    添加favicon

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const fs = require('fs');
-// const proxy = require('proxy-middleware');
+const favicon = require('serve-favicon');
 
 const dbUrl = require('./config/db').url;
 
@@ -39,7 +39,7 @@ app.use(session({
 app.use(cookieParser(secret));
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
